fb4f677948f374be3d6b6be0a79e7a22ef11d3c8
duwenqi <1126035133@qq.com>
2022-04-17 17:18:16 +0800

    更换了数据库软件“
    git status
    git commit -m 更换了数据库软件“

--- luffy/settings/dev.py
+++ luffy/settings/dev.py
@@ -90,10 +90,20 @@ WSGI_APPLICATION = 'luffy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'luffy',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'ah7',
+        'PASSWORD': 'dwq0627.'
     }
 }
 
